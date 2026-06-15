70b80fc938bf949976ea8e1aae2d2a2c8bb9d83e
PranavSharma112 <pranavsharma2904@gmail.com>
2026-06-15 14:37:36 +0530

    Introduction to Strings and application of basic mehtods

---
+++ chapter_02_variables_and_simple_data_types/TIY.py
@@ -0,0 +1,11 @@
+# 2.1 Simple message assigned using a variable and then printing it 
+message = "This is practice."
+print(message)
+
+# 2.2 Simple Messages: Assign a message to a variable, and print that message.
+# then change the value to a new variable to a new message, and print the new message 
+
+msg = "Hello Wolrd!"
+print(msg)
+msg = "Hello Python"
+print(msg)
---
+++ chapter_02_variables_and_simple_data_types/name.py
@@ -0,0 +1,20 @@
+# Starting With the data type lists it can be defined by double("") / Single quotes('') 
+# you can use them one inside the other for emphasis
+
+"This is a string."
+'This is also a string.'
+
+'I told my friend, "Python is my favourite language!"'
+"The language 'Python' is named after Monty Pyhton, not the snake."
+"One of Python's stengths is its diverse and supportive community."
+
+name  = "ada lovelace"
+print(name.title())    # .title() changes first letter of each word to uppercase
+# here we happend to invoke method . symbolizes to which variable name to act upon and parenthesis to give additional information
+
+print(name.upper()) # .upper() changes the entire string to upper case
+print(name.lower()) # .lower() changes the entire string to lower case
+
+# lower case happens to be particularly useful for storing and sorting data
+
+
