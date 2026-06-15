67c1c98b3c51695a13cf757dc7c6c3470857b283
PranavSharma112 <pranavsharma2904@gmail.com>
2026-06-16 01:57:41 +0530

    Format String, Striping, Suffix & Prefix

--- chapter_02_variables_and_simple_data_types/full_name.py
+++ chapter_02_variables_and_simple_data_types/full_name.py
@@ -7,4 +7,35 @@ print(full_name)
 
 print(f"Hello, {full_name.title()}")
 
+print("python")
+
+
+# Spacing using backslash \n for new line and \t for a tab space
+print("\tPython")
+
+print("Languages:\nPython\nC\nJavaScript")
+
+print("Languages:\n\tPython\n\tC\n\tJavaScript")
+
+#  Stripping Whitespaces
+
+favorite_language = ' python '
+print(favorite_language)
+print(favorite_language.rstrip())
+print(favorite_language.lstrip())
+print(favorite_language.strip())
+# Striping functions are used to clean data prior storing
+
+# Removing Prefixes
+nostarch_url = 'https://nostarch.com'
+nostarch_url.removeprefix('https://')
+print(nostarch_url)
+simple_url = nostarch_url.removesuffix('.com')
+print(simple_url)
+
+
+
+
+
+
 
