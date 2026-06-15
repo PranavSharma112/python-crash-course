0fe47fdd9ad838cebeac1a0db4e56578a8731bc3
PranavSharma112 <pranavsharma2904@gmail.com>
2026-06-16 02:03:56 +0530

    Conditions for Apostrophe use

---
+++ chapter_02_variables_and_simple_data_types/apostrophe.py
@@ -0,0 +1,5 @@
+# Avoiding Syntax Erros with Strings
+message = "One of Python's strengths is its diverse community."
+print(message)
+
+# if the apostrophe had been in single quotes it would have caused errors 
