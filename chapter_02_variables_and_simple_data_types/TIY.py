8652d57ee96b109a5fdfa3a4600dafbdc4a12030
PranavSharma112 <pranavsharma2904@gmail.com>
2026-06-16 02:26:46 +0530

    Solved till Q2.5

--- chapter_02_variables_and_simple_data_types/TIY.py
+++ chapter_02_variables_and_simple_data_types/TIY.py
@@ -9,3 +9,26 @@ msg = "Hello Wolrd!"
 print(msg)
 msg = "Hello Python"
 print(msg)
+
+# 2.3 Personal Message: Use a variable to represent a person's name,and print a message
+# a message to that person. your message should be simple, such as, 
+# "Hello Eric, Would You like to learn some Python today?"
+
+Erics_message = "Hello Eric, Would You like to learn some Python today?"
+print(Erics_message)
+
+# 2.4 Name Cases: Use a variable to represent a person then print their name in uppercase, lowercase and title
+name = "James bond"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+# 2.5 Famous Quote: Find a Quote from a famous person you admire. Print the quote and 
+# the name of the author. Your output  should look something like the following, including the quotation marks:
+# Albert Einstein once said, "A person who never made a mistake never tried anything new"
+
+NameOfFamousPerson = "Marcus Aurelius"
+Quote = "Become indifferent to what makes no difference"
+
+print(f'{NameOfFamousPerson} once said, "{Quote}"')
+
--- chapter_02_variables_and_simple_data_types/apostrophe.py
+++ chapter_02_variables_and_simple_data_types/apostrophe.py
@@ -1,4 +1,4 @@
-# Avoiding Syntax Erros with Strings
+# Avoiding Syntax Errors with Strings
 message = "One of Python's strengths is its diverse community."
 print(message)
 
