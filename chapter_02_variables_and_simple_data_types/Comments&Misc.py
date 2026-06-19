02e2e45d34d480bc59822aa6bc44e8f483c639d9
PranavSharma112 <pranavsharma2904@gmail.com>
2026-06-19 10:51:48 +0530

    Some things to keep in mind

---
+++ chapter_02_variables_and_simple_data_types/Comments&Misc.py
@@ -0,0 +1,21 @@
+# Say hello to Everyone.
+print("Hello Python people!")
+
+
+# REASON TO COMMENT: expalin code what is does and how it does, u wont lose a day even you come back to it after a long while,
+# write meaningful comments
+# Descriptive so that others can get it 
+# clear
+# concise 
+# when to write , if multiple approaches had to taken before deciding then...
+
+# Numbers 0.2 + 0.1 can give large decimal numbers with a marginal error because of the way they are stored internally
+# Division always return float, float any no. with a decimal in Python.
+# Underrated Underscores: u can use underscores between digits of a large no. to make it readable
+# Multiple assignments: x, y, z = 0, 0, 0 
+# industry practice to use ALL CAPS FOR NAMES OF GLOBAL VARIABLES = 10X
+
+
+import this
+
+
