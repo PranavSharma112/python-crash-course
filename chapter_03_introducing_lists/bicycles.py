ce6a16c9b130c689d85a30b8e15099fb1062a750
PranavSharma112 <pranavsharma2904@gmail.com>
2026-06-25 01:16:46 +0530

    Starting with lists.

---
+++ chapter_03_introducing_lists/bicycles.py
@@ -0,0 +1,17 @@
+bicycles = ['trek','cannondale','redline','specialized']
+print(bicycles)
+
+# Accessing the elements in the list
+print(bicycles[3].title())
+print(bicycles[-1].title())
+
+# Using individual Values form a list 
+message = f"My first bicycle was a {bicycles[0].title()}"
+
+
+
+
+
+
+
+
