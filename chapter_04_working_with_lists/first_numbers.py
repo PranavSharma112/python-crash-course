10b9015e3d59cb1aebf3528f85df541bc72a910e
PranavSharma112 <pranavsharma2904@gmail.com>
2026-06-27 03:15:09 +0530

    Range Function.

---
+++ chapter_04_working_with_lists/first_numbers.py
@@ -0,0 +1,5 @@
+for value in range(1,6):
+    print(value)
+
+# range functions starts from 0 and goes till n-1
+# it also accepts only one parameter the upper bound and lowerbound is assumed to be 0
