e1f7234ac69c3ceb37d9309b259c32e3d3aa60ca
PranavSharma112 <pranavsharma2904@gmail.com>
2026-06-27 03:17:55 +0530

    List of numbers using range function.

--- chapter_04_working_with_lists/first_numbers.py
+++ chapter_04_working_with_lists/first_numbers.py
@@ -1,5 +1,8 @@
 for value in range(1,6):
     print(value)
 
+
+numbers = list(range(1,6))
+print(numbers)
 # range functions starts from 0 and goes till n-1
 # it also accepts only one parameter the upper bound and lowerbound is assumed to be 0
